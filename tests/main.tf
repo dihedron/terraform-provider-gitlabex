274f2fc344d436aa1e7e6977092004dbf1a55748
Andrea Funtò <dihedron.dev@gmail.com>
2017-05-18 14:43:42 +0200

    Updated tests.

--- tests/main.tf
+++ tests/main.tf
@@ -1,22 +1,65 @@
-variable "tenant" {
-  type        = "string"
-  default     = "tenant-x"
+/*
+ * The name of the project as a variable.
+ */
+variable "project" {
+  type        		= "string"
+  default     		= "mynewapp"
 }
 
-provider "zeus_gitlab" {
-#	token       		= ""
-	base_url 			  = "http://localhost/api/v3/"
+/*
+ * The GitLab provider.
+ */
+provider "gitlabx" {
+#	token			= "<token goes here>"
+	base_url 		= "http://localhost/api/v3/"
 }
 
-resource "zeus_gitlab_group" "group01" {
-	name = "group01"
-	path ="path01"
-	description="my first test group"
+/*
+ * The tenant's project group.
+ */
+resource "gitlabx_group" "group" {
+	name 				= "${var.project}"
+	path 				= "${var.project}-group"
+	description			= "The GitLab projects group for the ${var.project} application"
+	visibility_level 	= "private"
 }
 
-resource "zeus_gitlab_project" "infrastructure-repo" {
-	name        		= "${var.tenant}-infrastructure"
-	path				= ${zeus_gitlab_group.group1.path}/${var.tenant}-infrastructure"
-	description 		= "The infrastructure configuration GitLab repository"
-	visibility_level 	= "public"
+/*
+ * The infrastructure definitions repository.
+ */ 
+resource "gitlabx_project" "infrastructure" {
+	name        		= "${var.project}-infrastructure"
+	namespace_id		= "${gitlabx_group.group.id}"
+	description 		= "The infrastructure configuration repository for the ${var.project} "
+	visibility_level 	= "private"
+}
+
+/*
+ * The first microservice.
+ */
+resource "gitlabx_project" "microservice-01" {
+	name        		= "${var.project}-microservice-01"
+	namespace_id		= "${gitlabx_group.group.id}"
+	description 		= "The source repository for the first microservice in ${var.project}"
+	visibility_level 	= "private"
+}
+
+/*
+ * The second microservice.
+ */
+resource "gitlabx_project" "microservice-02" {
+	name        		= "${var.project}-microservice-02"
+	namespace_id		= "${gitlabx_group.group.id}"
+	description 		= "The source repository for the second microservice in ${var.project}"
+	visibility_level 	= "private"
+}
+
+/*
+ * The third microservice.
+ */
+resource "gitlabx_project" "microservice-03" {
+	name        		= "${var.project}-microservice-03"
+	namespace_id		= "${gitlabx_group.group.id}"
+	description 		= "The source repository for the third microservice in ${var.project}"
+	visibility_level 	= "private"
 }
